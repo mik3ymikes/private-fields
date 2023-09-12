b77e3d49d22759bb051df46c3660b31af9be44f1
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-09-11 22:01:15 -0500

    Working on random number and getter issue

--- person.js
+++ person.js
@@ -1,5 +1,26 @@
 class Person {
-  // Write code here
+  #id
+
+  constructor(){
+    this.#id=Math.floor(Math.random()*100)+1
+  }
+
+  get id(){
+    return this.#id
+  }
+
 }
+
+const person1=new Person();
+const person2=new Person();
+
+
+console.log(person1.id)
+console.log(person2.id)
+
+
+
+
+
 
 module.exports = Person;
